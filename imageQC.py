ae3c11b7bc6c7697d276ab1dcc23ac9e730d8690
DESKTOP-M80B50R\Dr. S Chakraborty <sudip.embedded@gmail.com>
2025-12-17 17:16:07 +0530

    check with rotated image. know rotation 25 deg, and algorihmic rotation found 25.08 deg

--- imageQC.py
+++ imageQC.py
@@ -2,7 +2,7 @@ import filter as fltr
 import image as img
 #########################################################################################
 if __name__ == "__main__":
-    image_path = "CO2022007_00028.TIF"
+    image_path ="Rotated_Page_25_Deg.tif" # "CO2022007_00028.TIF"
     img_gray=img.to_gray(image_path)
     img_no_noise=img.remove_noise(img_gray)
     img_final, skew_angle = img.deskew(img_no_noise)
